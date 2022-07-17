8b1055c32a06e3c6abfbf76f5837cdb4ac0777db
Mihail Slavov <inj.slavov@gmail.com>
2022-07-17 17:23:21 +0300

    Basic Syntax, Conditional Statements and Loops - Lab

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/P02Passed.java
@@ -0,0 +1,14 @@
+package BasicSyntaxConditionalStatementsandLoopsLab;
+
+import java.util.Scanner;
+
+public class P02Passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.0) {
+            System.out.println("Passed!");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/P03PassedOrFailed.java
@@ -0,0 +1,16 @@
+package BasicSyntaxConditionalStatementsandLoopsLab;
+
+import java.util.Scanner;
+
+public class P03PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.0) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
