3ced1e0f3b720af1ab24adfe4284d56e62a2b661
Mihail Slavov <mihail_slavov@icloud.com>
2022-07-19 19:16:18 +0300

    Basic Syntax, Conditional Statements and Loops - Lab

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/P07TheatrePromotion.java
@@ -0,0 +1,12 @@
+package BasicSyntaxConditionalStatementsandLoopsLab;
+
+import java.util.Scanner;
+
+public class P07TheatrePromotion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
